9172e13b447f7d5bf780fcf4acc4e87225e56ee9
DELL-PC\hyesoo <cto@kidsoft.kr>
2020-06-04 12:29:29 +0900

    add comboBox

--- module/uartConnectDialog.py
+++ module/uartConnectDialog.py
@@ -7,15 +7,28 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import serial.tools.list_ports
+from PyQt5.QtSerialPort import QSerialPort
 
 class Ui_uartDialog(object):
 
+    BAUDRATES = (
+        QSerialPort.Baud1200,
+        QSerialPort.Baud2400,
+        QSerialPort.Baud4800,
+        QSerialPort.Baud9600,
+        QSerialPort.Baud19200,
+        QSerialPort.Baud38400,
+        QSerialPort.Baud57600,
+        QSerialPort.Baud115200,
+    )
+
     def setupUi(self, uartDialog):
-        uartDialog.setObjectName("Dialog")
+        uartDialog.setObjectName("uartDialog")
         uartDialog.resize(420, 200)
         uartDialog.setMinimumSize(QtCore.QSize(420, 200))
         uartDialog.setMaximumSize(QtCore.QSize(420, 200))
-        self.uart_frame = QtWidgets.QFrame(Dialog)
+        self.uart_frame = QtWidgets.QFrame(uartDialog)
         self.uart_frame.setGeometry(QtCore.QRect(10, 10, 391, 171))
         self.uart_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.uart_frame.setFrameShadow(QtWidgets.QFrame.Plain)
@@ -29,37 +42,41 @@ class Ui_uartDialog(object):
         self.save_pushButton = QtWidgets.QPushButton(self.uart_frame)
         self.save_pushButton.setGeometry(QtCore.QRect(130, 140, 121, 23))
         self.save_pushButton.setObjectName("save_pushButton")
-        self.port_lineEdit = QtWidgets.QLineEdit(self.uart_frame)
-        self.port_lineEdit.setGeometry(QtCore.QRect(100, 50, 231, 20))
-        self.port_lineEdit.setObjectName("port_lineEdit")
-        self.rate_lineEdit = QtWidgets.QLineEdit(self.uart_frame)
-        self.rate_lineEdit.setGeometry(QtCore.QRect(100, 90, 231, 20))
-        self.rate_lineEdit.setObjectName("rate_lineEdit")
         self.rate_label = QtWidgets.QLabel(self.uart_frame)
         self.rate_label.setGeometry(QtCore.QRect(40, 90, 31, 16))
         self.rate_label.setObjectName("rate_label")
+        self.port_comboBox = QtWidgets.QComboBox(self.uart_frame)
+        self.port_comboBox.setGeometry(QtCore.QRect(100, 50, 231, 22))
+        self.port_comboBox.setObjectName("port_comboBox")
+        self.rate_comboBox = QtWidgets.QComboBox(self.uart_frame)
+        self.rate_comboBox.setGeometry(QtCore.QRect(100, 90, 231, 22))
+        self.rate_comboBox.setObjectName("rate_comboBox")
+
+        # 연결 가능한 port comboBox에 추가하기
+        comlist = serial.tools.list_ports.comports()
+        self.port_comboBox.addItem('')
+        for element in comlist:
+            self.port_comboBox.addItem(element.device)
+
+        self.rate_comboBox.insertItems(0, [str(x) for x in self.BAUDRATES])
 
         self.retranslateUi(uartDialog)
         QtCore.QMetaObject.connectSlotsByName(uartDialog)
 
         self.save_pushButton.clicked.connect(lambda: self.saveAddress(uartDialog))
 
-    def retranslateUi(self, Dialog):
+
+    def retranslateUi(self, uartDialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Connect Mode"))
+        uartDialog.setWindowTitle(_translate("Dialog", "Connect Mode"))
         self.wifi_label_6.setText(_translate("Dialog", "Uart mode"))
         self.port_label.setText(_translate("Dialog", "Port"))
         self.save_pushButton.setText(_translate("Dialog", "Save"))
         self.rate_label.setText(_translate("Dialog", "Rate"))
 
 
-
     def saveAddress(self,uartDialog):
-        port = self.port_lineEdit
-        if len(port) ==3:
-            uartDialog.reject()
-        else:
-            uartDialog.accept()
+        uartDialog.accept()
 
 
 '''
@@ -67,11 +84,10 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
-    ui = Ui_uartDialog()
+    ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
 
 '''
-
 
